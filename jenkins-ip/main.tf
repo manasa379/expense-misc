5bd9e25a5cef7d56f6421a0f158cfec324dd2c89
DELL <manasajalagam379@gmail.com>
2024-08-03 21:10:14 +0530

    Expense project

--- jenkins-ip/main.tf
+++ jenkins-ip/main.tf
@@ -21,7 +21,7 @@ resource "aws_route53_record" "jenkins" {
 
 
 data "aws_instance" "artifactory" {
-  instance_id = "i-0a5a01a28b948d040"
+  instance_id = "i-06bd5ae95d6121c47"
 }
 
 resource "aws_route53_record" "artifactory" {
